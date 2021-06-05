46782e6b2eede11edf445fa1bbbf38e693a0cd9e
praveen-malviya <contact.praveenmalviya@gmail.com>
2021-06-06 03:10:45 +0530

    Added: Blogs Pages

--- src/Home.js
+++ src/Home.js
@@ -2,39 +2,24 @@ import { useState } from "react";
 
 const Home = () => {
 
-    
-    const [text, setText] = useState("");
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
 
-    function clicked (e){
-
-        var text = "button clicked: Here I Am";
-
-        setText(text);
-
-        console.log(e)      
-    }
-
-    function nowclickme(params) {
-        var text = "button clicked: "+params;
-
-        setText(text);
-    }
-
+    ])
     return ( 
 
-     
-        <div className="content">
+        <div className="home">
+            {blogs.map((blog) => (
+               <div className="blog-preview" key={blog.id}>
 
-            <h1>Home Page</h1>
+                <h2>{ blog.title }</h2>
+                <p>Written By { blog.body }</p>
 
-            <button onClick={clicked}>Click me</button>
-
-            <button
-                onClick= {() => nowclickme('mario')}
-            >Nowclickme</button>
-
-            <p>{text}</p>
-
+                </div>
+            ))}
+          
         </div>
 
      );
