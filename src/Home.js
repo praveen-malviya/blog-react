6d6b1e88a530126b53ddaaa5ee3ab5444c643fae
praveen-malviya <contact.praveenmalviya@gmail.com>
2021-06-06 23:03:22 +0530

    CH12: Props and Filtering

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,21 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
+        { title: 'React tutorial', body: 'lorem ipsum...', author: 'praveen', id: 3 }
 
     ])
     return ( 
 
         <div className="home">
-            {blogs.map((blog) => (
-               <div className="blog-preview" key={blog.id}>
-
-                <h2>{ blog.title }</h2>
-                <p>Written By { blog.body }</p>
-
-                </div>
-            ))}
+          
+        <BlogList blogs={ blogs } title = "All Blogs" /> ,<br />
+        <BlogList blogs={ blogs.filter((blog) => blog.author === 'mario') } title = "Filter Blogs" />
           
         </div>
 
