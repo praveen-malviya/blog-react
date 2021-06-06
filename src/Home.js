ef3ac99ac8634d006f893ad27cf137fe467143c5
praveen-malviya <contact.praveenmalviya@gmail.com>
2021-06-07 04:49:03 +0530

    Use Hook: UseEffect
    use of it and dependency.

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
+
+    const [name, setName] = useState('Praveen');
 
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
         { title: 'React tutorial', body: 'lorem ipsum...', author: 'praveen', id: 4 }
-])
+]);
 
     const handleDelete = (id) => {
-        const newBlogs = blogs;
-        setBlogs(newBlogs.filter((blog) => blog.id !== id))
-
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
     }
-    return ( 
+
+    useEffect(() => {
+        console.log('Rendred');
+        console.log(name);
+        
+    }, [name])
+    
+    
+    return( 
 
         <div className="home">
           
         <BlogList blogs={ blogs } title = "All Blogs" handleDelete = { handleDelete } /> ,<br />
-        {/* <BlogList blogs={ blogs.filter((blog) => blog.author === 'mario') } title = "Filter Blogs" /> */}
+        
+        <button onClick={() => setName('Malviya')}>Change Name</button>
+        <p>{ name }</p>
           
         </div>
 
