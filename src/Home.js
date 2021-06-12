6a11faf3130a5a98f2a21b6bbe531dc0ff3002e2
praveen-malviya <contact.praveenmalviya@gmail.com>
2021-06-13 02:47:45 +0530

    CH20: Custom HOOK
    We make a Fetch function in our Custom hooks so now we need to write only one Line

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,14 @@
-import { useEffect, useState } from "react";
+
 import BlogList from "./BlogList";
+import useFetch from "./useFetch"
 
 const Home = () => {
-
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [gotError, setGotError] = useState();
-        
-    
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok){
-                        throw Error('Could not find the resources you requested for');
-                    }
-                    return res.json();
-                })
-                .then(data =>{
-                    setBlogs(data);
-                    setIsPending(false);
-                    setGotError(null);
-                })
-                .catch(err => {
-                    setIsPending(false);
-                    setGotError(err.message);
-                    
-                })
-        }, 1000);  
-    }, [])
-    
-    
+const {data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
+     
     return( 
 
         <div className="home">
-        { gotError && <h4> { gotError } </h4>}
+        { error && <h4> { error } </h4>}
         { isPending && <h3>Loading.....</h3>} 
         { blogs && <BlogList blogs={ blogs } title = "All Blogs"/> }
         
