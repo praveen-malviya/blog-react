cc2128f1c47341571c9c1790b431bc88670a8838
praveen-malviya <contact.praveenmalviya@gmail.com>
2021-06-13 02:09:10 +0530

    Ch19: catch Error updated

--- src/Home.js
+++ src/Home.js
@@ -5,30 +5,38 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
-
-
+    const [gotError, setGotError] = useState();
+        
+    
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data =>{
-                setBlogs(data);
-                setIsPending(false);
-            })
-
-        }, 1000);
-        
+                .then(res => {
+                    if(!res.ok){
+                        throw Error('Could not find the resources you requested for');
+                    }
+                    return res.json();
+                })
+                .then(data =>{
+                    setBlogs(data);
+                    setIsPending(false);
+                    setGotError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setGotError(err.message);
+                    
+                })
+        }, 1000);  
     }, [])
     
     
     return( 
 
         <div className="home">
-
-        { isPending && <h4>Loading.....</h4>} 
+        { gotError && <h4> { gotError } </h4>}
+        { isPending && <h3>Loading.....</h3>} 
         { blogs && <BlogList blogs={ blogs } title = "All Blogs"/> }
         
           
