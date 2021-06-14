4b9498d29ced60bcbfacb807d1bbf5d44ad509cc
praveen-malviya <contact.praveenmalviya@gmail.com>
2021-06-14 21:43:51 +0530

    CH26: Blog Body added.
    added Blog body component now a full blog page can be load from json server

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,25 @@
 import { useParams } from "react-router-dom";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch"
 
 const BlogDetails = () => {
 
     const { id } = useParams();
 
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/'+ id);
+
     return ( 
         <div className="blog-details">
-            <h2> Blog Details -- { id }</h2>
+        { error && <p> { error } </p>}
+        { isPending && <h3> Loading...</h3>}
+        
+       { blog && <article>
+        <h2>{ blog.title}</h2>
+        <h5> Wriiten By { blog.author}</h5>
+       <p> {blog.body} </p> 
+       </article>}
         </div>
+
      );
 }
  
